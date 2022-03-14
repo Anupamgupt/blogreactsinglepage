38275f10ee5fb52305b166ec3ead07e9cba9c550
Anupam Kumar Gupta <91828258+Anupamgupt@users.noreply.github.com>
2022-03-14 13:11:54 +0530

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-import ReactDom from 'react-dom'
-import App from './App'
-// import Blogpage from './Blogpage'
-
-ReactDom.render(<App/>,document.getElementById('root'))
