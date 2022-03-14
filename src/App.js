2adc198b32e85ef355851b331e83fcd938c41d71
Anupam Kumar Gupta <91828258+Anupamgupt@users.noreply.github.com>
2022-03-14 13:29:51 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import React from 'react'
+import Blogpage from './blogsinglepage/Blogpage'
+import Totalviews from './blogsinglepage/Totalviews'
+import Previous from './blogsinglepage/Previous'
+
+const App = () => {
+  return (
+    <>
+        <Blogpage/>
+        <Totalviews/>
+        <Previous/>
+    </>
+    
+  )
+}
+
+export default App
